517c649c1398bea29770702d3a8404e3920e54de
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-20 18:45:36 +0100

    feat: add form modeling

--- src/Form.elm
+++ src/Form.elm
@@ -17,3 +17,36 @@ database =
     , Credentials "roman@rmail.com" "roman"
     , Credentials "bun_bun@bmail.com" "bun_bun"
     ]
+
+
+type PasswordError
+    = PasswordEmpty
+    | PasswordToShort
+    | PasswordToLong
+    | PasswordHasNoSpecialCharacters
+    | PasswordHasNoNumbers
+    | PasswordHasNoCapitalLetters
+
+
+type PasswordInput
+    = PasswordDefaultValue String
+    | PasswordValid { value : Result (List PasswordError) String }
+
+
+type EmailError
+    = EmailEmpty
+    | EmailInvalid
+
+
+type EmailInput
+    = EmailDefaultValue String
+    | EmailValid { value : Result (List EmailError) String }
+
+
+type Form
+    = Form { email : EmailInput, password : PasswordInput }
+
+
+init : Form
+init =
+    Form { email = EmailDefaultValue "", password = PasswordDefaultValue "" }
