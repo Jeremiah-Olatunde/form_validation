59549707436fb6782812948a7d4036e7b1a2363e
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-20 18:25:20 +0100

    feat: add mock database

--- src/Form.elm
+++ src/Form.elm
@@ -2,4 +2,18 @@ module Form exposing (..)
 
 import Html exposing (text)
 
-main = text "hello world"
+
+main =
+    text "hello world"
+
+
+type alias Credentials =
+    { email : String, password : String }
+
+
+database : List Credentials
+database =
+    [ Credentials "jeremiah@jmail.com" "jeremiah"
+    , Credentials "roman@rmail.com" "roman"
+    , Credentials "bun_bun@bmail.com" "bun_bun"
+    ]
